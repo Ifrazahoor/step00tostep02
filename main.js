1ece4fd7180a9bc78d743422a00563cff49f8f20
Ifrazahoor <166019853+Ifrazahoor@users.noreply.github.com>
2024-07-02 13:57:34 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//helloworld
+/*let song = "Balle Balle";
+console.log(song);
+//syntax error//lett
+lett message = "hello world";
+console.log(message);
+//type error loger//console.log
+let name = "cutie";
+console.loger(name);
+//assignabilaty error//number not assign
+let message = "Hello World";
+message = 6;
+console.log(message);
+//strongly typed syntax//written a string,number,bolean
+let a : string = "iran";
+a = "pakistan";
+let b : number = 3;
+let c : boolean = false;
+
+//type inference//""initilise a value
+let e = "iran";
+let f = 10.0;
+f = 25;
+let g = true;
+g = true;*/
+//let ak hi name ke 2 varial nahi bante
+let myName = "ifra";
+//let myName="iqra"////example
+let myChoice = "shopping";
+console.log(myChoice);
+export {};
